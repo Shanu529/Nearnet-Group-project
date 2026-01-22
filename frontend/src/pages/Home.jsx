e43eacdb839cd8615a6db446cfae3fbe184ee4d5
Shanu Chhetri <shnau.chhetri529@gmail.com>
2026-01-22 12:51:03 +0530

    ui change

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,15 +8,15 @@ function Home() {
     <>
   
     <section className=" bg-white flex items-center  px-6 justify-center bg-[url(/main5.jpg)] bg-cover h-[90vh] py-20 sm:py-30 bg-center ">
-      <div className="max-w-4xl text-center ">
+      <div className="max-w-4xl sm:text-center ">
         <div className="inline-flex items-center gap-2 px-4 py-2 rounded-full bg-emerald-50 text-emerald-600 text-sm font-medium mb-6">
           <span className="text-lg">♡</span>
           Building Stronger Communities Together
         </div>
 
        
-        <h1 className="text-[1.6rem] sm::text-6xl lg:text-6xl font-extrabold text-white leading-tight mb-6">
-          Share Resources Within  <br />
+        <h1 className="text-[1.9rem] sm::text-6xl lg:text-6xl font-extrabold text-white leading-tight mb-6">
+          Share Resources Within
           Your{" "}
           <span className="bg-linear-to-r from-emerald-500  to-emerald-500 bg-clip-text text-transparent">
             Neighborhood
@@ -24,22 +24,22 @@ function Home() {
         </h1>
 
         
-        <p className="text-white text-[1rem] sm:text-lg  mx-auto ">
+        <p className="text-white text-[0.9rem] sm:text-lg  mx-auto ">
           Need a ladder for the weekend? Have a drill you rarely use?
           Connect with neighbors and share resources to build a stronger,
           more sustainable community.
         </p>
 
-      
-        <div className="flex flex-col sm:flex-row gap-4 justify-center mt-10">
-          <button className=" px-5 sm:px-8 py-3 sm:py-4 rounded-xl bg-emerald-700 text-white font-semibold shadow-md hover:bg-emerald-500 transition">
-           Request a Resource
-          </button>
+      <div className="flex flex-col sm:flex-row items-center justify-center gap-3 sm:gap-6 mt-8 sm:mt-10">
+         <button className="w-full sm:w-auto px-4 sm:px-10 py-2.5 sm:py-4 text-sm sm:text-lg rounded-xl bg-emerald-600 text-white font-semibold shadow-md hover:bg-emerald-500 active:scale-95 transition duration-200">
+          Request a Resource
+         </button>
 
-          <button className=" px-5 sm:px-8 py-3 sm:py-4 rounded-xl border-2 border-emerald-500 text-emerald-600 font-semibold hover:bg-emerald-50 transition">
-             Offer a Resource
-          </button>
-        </div>
+        <button className="w-full sm:w-auto px-4 sm:px-10 py-2.5 sm:py-4 text-sm sm:text-lg rounded-xl border-2 border-emerald-400 bg-white/90 text-emerald-700 font-semibold hover:bg-emerald-50 active:scale-95 transition duration-200">
+         Offer a Resource
+        </button>
+     </div>
+ 
 
       </div>
     </section>
