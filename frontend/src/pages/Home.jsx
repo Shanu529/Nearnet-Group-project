ca6a8d4a4b78ce9e8b4d569e769c3c2bd90bcb60
Shanu Chhetri <shnau.chhetri529@gmail.com>
2026-01-22 12:39:39 +0530

    bug fixed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
   return (
     <>
   
-    <section className=" bg-white flex items-center  px-6 justify-center bg-[url(/main5.jpg)] bg-cover h-full py-20 sm:py-30 bg-center ">
+    <section className=" bg-white flex items-center  px-6 justify-center bg-[url(/main5.jpg)] bg-cover h-[90vh] py-20 sm:py-30 bg-center ">
       <div className="max-w-4xl text-center ">
         <div className="inline-flex items-center gap-2 px-4 py-2 rounded-full bg-emerald-50 text-emerald-600 text-sm font-medium mb-6">
           <span className="text-lg">♡</span>
@@ -15,7 +15,7 @@ function Home() {
         </div>
 
        
-        <h1 className="text-[1.6rem] sm::text-6xl font-extrabold text-white leading-tight mb-6">
+        <h1 className="text-[1.6rem] sm::text-6xl lg:text-6xl font-extrabold text-white leading-tight mb-6">
           Share Resources Within  <br />
           Your{" "}
           <span className="bg-linear-to-r from-emerald-500  to-emerald-500 bg-clip-text text-transparent">
